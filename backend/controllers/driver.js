602f0269c5d5b00191a7d48dd1c64bcc7589c830
chayan101 <chayan.g101@gmail.com>
2021-04-19 23:28:37 +0530

    dlogin updated

--- backend/controllers/driver.js
+++ backend/controllers/driver.js
@@ -4,60 +4,57 @@ const { check, validationResult } = require("express-validator");
 const {checkBookride, alterflag, getPendingRides} = require("../functions/functions");
 
 
-exports.dlogin = async (req,res) =>{
-	try
-	{
-		if(req.cookies.role === undefined && req.cookies.username === undefined){
-	    	res.redirect("/login")
-	  	}else{
-	  		var sql  = "select username from driver where username=?";
-		    await con.query(sql, [req.cookies.username],async function (err, result){
-		        
-		        try{
-		        				if (err)
-		        		        {  
-		        		        	res.send(err);
-		        		        }
-		        	
-		        		        if(result.length === 0){
-		        		          res.redirect("/login");
-		        		        }else{
-		        		        	// var result2;
-		        		        	try{
-
-										var sql = "Select * from pendingrides limit 1";
-										await con.query(sql, function (err, result)
-										{
-										 	if (err){
-										      console.log(error);
-	  										  res.sendStatus(500);
-										    }
-										   	if(!checkBookride() && result.length === 0){
-		        								res.status(200).render("driver", {flag:1},{async: true});
-		        							}else{
-		        								res.status(200).render("driver",{flag:4},{async: true});
-			        						}
-										});
-										}catch(error){
-											console.log(error);
-											res.sendStatus(500);
-										}
-		        						// console.log(result2);
-		        		          	
-		        					// res.status(200).render("driver", {flag:1});
-		        		        }
-		        		    }catch(error){
-		        		    	console.log(error);
-	  							res.sendStatus(500);
-		        		    }
-
-		    });
-	  	}
-	  }catch(error){
-	  	console.log(error);
-	  	res.sendStatus(500);
-	  }
-}
+// exports.dlogin = async (req,res) =>{
+// 	try
+// 	{
+// 		if(req.cookies.role === undefined && req.cookies.username === undefined){
+// 	    	res.redirect("/login")
+// 	  	}else{
+// 	  		var sql  = "select username from driver where username=?";
+// 		    await con.query(sql, [req.cookies.username],async function (err, result){
+// 		        				if (err)
+// 		        		        {
+// 													console.log(err);
+// 		        		        	res.send(err);
+// 		        		        }
+//
+// 		        		        if(result.length === 0){
+// 		        		          res.redirect("/login");
+// 		        		        }else{
+//
+// 										var sql2 = "Select * from pendingrides limit 1";
+// 										await con.query(sql2, function (err, result2)
+// 										{
+// 										 	if (err){
+// 										      console.log(error);
+// 	  										  res.sendStatus(500);
+// 										    }
+// 										   	if(!checkBookride() && result2.length === 0){
+// 		        								res.status(200).render("driver", {flag:1},{async: true});
+// 		        							}else{
+// 		        								res.status(200).render("driver",{flag:4},{async: true});
+// 			        						}
+// 										});
+// 										}catch(error){
+// 											console.log(error);
+// 											res.sendStatus(500);
+// 										}
+// 		        						// console.log(result2);
+//
+// 		        					// res.status(200).render("driver", {flag:1});
+// 		        		        }
+// 		        		    }catch(error){
+// 		        		    	console.log(error);
+// 	  							res.sendStatus(500);
+// 		        		    }
+//
+// 		    });
+// 	  	}
+// 	  }catch(error){
+// 	  	console.log(error);
+// 	  	res.sendStatus(500);
+// 	  }
+// }
 
 exports.pending = async (req, res) => {
 	try{
@@ -79,4 +76,44 @@ exports.pending = async (req, res) => {
 	console.log(error);
 	res.sendStatus(500);
 }
+};
+
+exports.dlogin = async(req,res)=>{
+	if(req.cookies.role === undefined && req.cookies.username === undefined){
+			res.redirect("/login")
+	}else{
+		try{
+			var sql  = "select username from driver where username=?";
+			await con.query(sql,[req.cookies.username], async (err,result)=>{
+				if(err){
+							console.log(err);
+							res.send(err);
+				}else if(result.length === 0){
+					//invalid username
+					res.redirect("/login");
+				}else{
+					var sql2 = "Select * from pendingrides limit 1";
+					try{
+					await con.query(sql2, (err, result2)=>{
+						if(err){
+							 console.log(err);
+							 res.sendStatus(500);
+						}else if(!checkBookride() && result2.length === 0){
+							res.status(200).render("driver", {flag:1},{async: true});
+						}else{
+							res.status(200).render("driver",{flag:4},{async: true});
+						}
+					});
+				}catch(error){
+					console.log(error);
+					res.sendStatus(500);
+				}
+				}
+			});
+		}catch(error){
+			console.log(error);
+			res.sendStatus(500);
+		}
+	}
+
 };
